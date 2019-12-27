789f1f75e694da86154d5039c719e0a1ca2af63d
COE-006 <coe-006@coe-006s-imac.local>
2019-12-27 14:38:05 +0530

    Initial Commit

---
+++ UIRVI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIRVI
+//
+//  Created by COE-006 on 27/12/19.
+//  Copyright © 2019 COE-006. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
